d4799b80f04fcb905a8de431d8c8f01faba74f8d
h131177 <131177@stud.hvl.no>
2019-11-11 23:48:45 +0100

    Oppgave 4 ferdig

--- src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
@@ -11,7 +11,20 @@ public class SkrivBlogg {
 	private static String MAPPE = System.getProperty("user.dir") + "/src/no/hvl/dat100/jplab12/tests/";
 
 	public static boolean skriv(Blogg samling, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		try {
+			PrintWriter skriver = new PrintWriter(MAPPE + filnavn);
+			for(int i = 0; i < samling.getAntall(); i++) {
+				skriver.print(samling.getSamling()[i].toString());
+			}
+			skriver.close();
+			return true;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 }
